d51350b4df2333486ed19a7389edca1f30a6ad80
Borivoj Grujicic <borivoj.grujicic@gmail.com>
2024-05-14 17:44:00 +0200

    models removals

--- src/models/correlationstats.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct CorrelationStats{
-    pub true_count: i32,
-    pub false_count: i32,
-    pub percent_true: f64,
-    pub percent_false: f64
-}
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,8 +1,4 @@
-pub mod hremployees;
-pub mod currencydata;
 pub mod ordersreport;
 pub mod customerbyyear;
 pub mod topperformers;
 pub mod saleschoropleth;
-pub mod correlation;
-pub mod correlationstats;
